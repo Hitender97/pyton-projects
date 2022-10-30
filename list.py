1d8dfd4b17c88b859772c449f51d4a7a54370840
Hitender97 <pahujahitender@gmail.com>
2022-10-31 01:41:49 +0530

    updated basic

---
+++ list.py
@@ -0,0 +1,7 @@
+thisislist = ['orange','apple','watermelon','kiwi']
+thisislist.sort()
+print (thisislist)
+thisislist.reverse()
+print (thisislist)
+thisislist[1]='cherry'
+print(thisislist)
---
+++ pylogic.py
@@ -0,0 +1,7 @@
+from turtle import done
+
+
+i = 0
+
+if i <= 1000:
+    print ("I love You")
